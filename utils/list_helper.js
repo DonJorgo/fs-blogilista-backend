94f90ce8e56af0a3a9800625f039765cfdfd435b
Jorma Nieminen <donjorgo@gmail.com>
2021-07-02 20:18:36 +0100

    4.4: apufunktioita ja yksikkötestejä, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const sum = (acc, blog) => acc + blog.likes
+
+  return blogs.reduce(sum, 0)
+}
+
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }

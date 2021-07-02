2a9df26a9bb348783c970ce1c1aec8977cbb6857
Jorma Nieminen <donjorgo@gmail.com>
2021-07-02 18:03:59 +0100

    4.2 blogilista, step2 - separate index.js and app.js

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
 
-const blogRouter = require('./controllers/blogs')
+const server = http.createServer(app)
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
-
-app.use('/api/blogs', blogRouter)
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })

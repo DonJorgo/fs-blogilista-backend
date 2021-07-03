117f68ae1c2672fa3ed195a4ee0d821e0806d319
Jorma Nieminen <donjorgo@gmail.com>
2021-07-03 14:57:10 +0100

    4.8: blogilistan testit, step 1 - separate environments for deve, test and prod

--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-undef */
 require('dotenv').config()
 
-// eslint-disable-next-line no-undef
 let PORT = process.env.PORT
-// eslint-disable-next-line no-undef
-let MONGODB_URI = process.env.MONGODB_URI
+let MONGODB_URI = process.env.NOSE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
 
 module.exports = {
   MONGODB_URI,

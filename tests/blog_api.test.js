f3456f7da0caa0ae3d767509ca0062814ede5c18
Jorma Nieminen <donjorgo@gmail.com>
2021-07-03 15:37:03 +0100

    4.8: blogilistan testit, step 1 - GET /api/blogs -testit

---
+++ tests/blog_api.test.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+describe('GET /api/blogs', () => {
+  test('returns blogs as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('returns correct amount of blogs', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
